d518e0f3dd4e8c66e7649c4b0c6aae65279bf531
e <791174722@qq.com>
2018-08-22 20:00:59 +0800

    推荐功能完成，热门新闻完成。基本功能全部实现，剩下CSS与div调整与优化。目前只有协同过滤，冷启动问题尚未解决。

--- Article/views.py
+++ Article/views.py
@@ -1,3 +1,9 @@
+from django.template import Library
+from django.template.defaultfilters import stringfilter
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import sqlite3
 from django.shortcuts import render
 from . import models
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
@@ -9,6 +15,64 @@ from .models import User as User
 from django.shortcuts import HttpResponse
 
 basepath = os.getcwd() + '\\Article\\templates\\'
+
+
+def normalizeRarings(rating, record):
+    m, n = rating.shape
+    rating_mean = np.zeros((m, 1))
+    rating_norm = np.zeros((m, n))
+    for i in range(m):
+        idx = record[i, :] != 0
+        rating_mean[i] = np.mean(rating[i, idx])
+        rating_norm[i, idx] -= rating_mean[i]
+    return rating_norm, rating_mean
+
+
+def recommend(request):
+    # 读入数据
+    conn = sqlite3.connect("news.sqlite")
+    ratings_df = pd.read_sql_query("select * from Article_score;", conn)
+    ratings_df.tail()
+    # 初始化用户与新闻信息，将Id做数组保存
+    userNo = ratings_df['user_id'].max() + 1
+    pageNo = ratings_df['article_id'].max() + 1
+    # 新闻评分矩阵，储存用户评价新闻的分数
+    rating = np.zeros((pageNo, userNo))
+    for index, row in ratings_df.iterrows():
+        rating[int(row['article_id']), int(row['user_id'])] = row['score']
+    record = rating > 0
+    # 评分记录矩阵，用01记录有评分的记录
+    record = np.array(record, dtype=int)
+    # 标准化
+    rating_norm, rating_mean = normalizeRarings(rating, record)
+    rating_norm = np.nan_to_num(rating_norm)
+    rating_mean = np.nan_to_num(rating_mean)
+    num_features = 10
+    x_p = tf.Variable(tf.random_normal([pageNo, num_features], stddev=0.35))
+    theta_p = tf.Variable(tf.random_normal([userNo, num_features], stddev=0.35))
+    loss = 1 / 2 * tf.reduce_sum(((tf.matmul(x_p, theta_p, transpose_b=True) - rating_norm) * record) ** 2) + \
+           1 / 2 * (tf.reduce_sum(x_p ** 2) + tf.reduce_sum(theta_p ** 2))
+    optimizer = tf.train.AdagradOptimizer(1e-4)
+    train = optimizer.minimize(loss)
+    tf.summary.scalar('loss', loss)
+    summaryMarged = tf.summary.merge_all()
+    filename = 'D:/page_t'
+    writer = tf.summary.FileWriter(filename)
+    sess = tf.Session()
+    init = tf.global_variables_initializer()
+    sess.run(init)
+    for i in range(5000):
+        _, movie_summary = sess.run([train, summaryMarged])
+        writer.add_summary(movie_summary, i)
+    c_x_p, c_t_p = sess.run([x_p, theta_p])
+    predicts = np.dot(c_x_p, c_t_p.T) + rating_mean
+    errors = np.sqrt(np.sum((predicts - rating) ** 2))
+    pages_df = pd.read_sql_query("select * from Article_article;", conn)
+    user_id = request.user.user_id
+    sortedResult = predicts[:, int(user_id)].argsort()[::-1]
+    predicts = np.transpose(predicts)
+    # 推荐完毕
+    return sortedResult
 
 
 def add_info(request, infos, data):
@@ -27,6 +91,8 @@ def add_info(request, infos, data):
 
 
 def detail(request, id):
+    hot_articles = models.article.objects.all().order_by("-click")
+    hot_articles = hot_articles[0:10]
     try:
         infos = models.article.objects.get(article_id=id)
     except Exception as e:
@@ -35,7 +101,7 @@ def detail(request, id):
         infos.click += 1
         infos.save()
     if not request.user.is_authenticated:
-        return render(request, 'pages/news.html', {'news': infos, 'selected': -1})
+        return render(request, 'pages/news.html', {'news': infos, 'selected': -1, 'hot_news': hot_articles})
     viewed = request.user.viewed
     request.user.viewed = add_info(request, infos, viewed)
     request.user.save()
@@ -45,10 +111,10 @@ def detail(request, id):
         score = models.Score.objects.create(user=request.user, article=infos, score=float(request.POST.get('score')))
         score.save()
         request.user.save()
-        return render(request, 'pages/news.html', {'news': infos, 'selected': 1})
+        return render(request, 'pages/news.html', {'news': infos, 'selected': 1, 'hot_news': hot_articles})
     if not scored or not str(id) in scored:
-        return render(request, 'pages/news.html', {'news': infos, 'selected': 0})
-    return render(request, 'pages/news.html', {'news': infos, 'selected': 1})
+        return render(request, 'pages/news.html', {'news': infos, 'selected': 0,  'hot_news': hot_articles})
+    return render(request, 'pages/news.html', {'news': infos, 'selected': 1, 'hot_news': hot_articles})
 
 
 def list(request):
@@ -207,6 +273,7 @@ def type(request, t):
 
 def index(request):
     Viewed = []
+    Recommend = []
     Result1 = []
     Result2 = []
     Result3 = []
@@ -232,14 +299,24 @@ def index(request):
         return render(request, 'pages/index.html',
                       {"type1": Result1, "type2": Result2, "type3": Result3, "type4": Result4, "type5": Result5})
     viewed = request.user.viewed
+    index = 0
+    recommend_id = recommend(request)
+    for i in recommend_id:
+        article = models.article.objects.get(article_id=int(i))
+        Recommend.append(article)
+        index += 1
+        if index == 10:
+            break;
     if request.user.viewed:
         article_list_id = viewed.split(',')
         for article_id in article_list_id:
             article = models.article.objects.get(article_id=int(article_id))
             Viewed.append(article)
+            if len(Viewed) > 9:
+                Viewed = Viewed[0:10]
     else:
         return render(request, 'pages/index.html',
                       {"type1": Result1, "type2": Result2, "type3": Result3, "type4": Result4, "type5": Result5})
     return render(request, 'pages/index.html',
                   {"type1": Result1, "type2": Result2, "type3": Result3, "type4": Result4, "type5": Result5,
-                   "viewed": Viewed})
+                   "viewed": Viewed, "recommend": Recommend})
