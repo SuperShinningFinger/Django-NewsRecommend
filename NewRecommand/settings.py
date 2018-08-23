f1444079dc32fbd5d273c0effaa35fd772059c99
max <791174722@qq.com>
2018-08-23 12:00:43 +0800

    修复了Linux兼容的templates not exist问题与Django2.1的语言编译问题

--- NewRecommand/settings.py
+++ NewRecommand/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'zh_Hans'
+LANGUAGE_CODE = 'zh-Hans'
 
 TIME_ZONE = 'UTC'
 
